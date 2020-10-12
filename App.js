852c96cc1a5cd773c11953443a492e2de7ebc82c
cl4pper <cl4pper@gmail.com>
2020-10-12 16:58:34 -0300

    feat: start to practice
    * receive item by text input
    * add new item into the list by clicking the button
    * show list bellow the button

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, updateText] = useState('');
+  const [list, handleList] = useState([]);
+
+  function clearInput() {
+    updateText('');
+  }
+
+  function addItem(item) {
+    let newList = [...list, item];
+    handleList(newList) ;
+  };
+
+  function updateList(item) {
+    addItem(item);
+    clearInput();
+    Alert.alert('List has been updated.');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.inputText}
+        placeholder="Add something..."
+        onChangeText={(value) => updateText(value)}
+        value={text}
+      />
+      <Button
+        style={styles.button}
+        title="Add item"
+        onPress={() => updateList(text)}
+      />
+      <View>
+        { list.map((item, index) => <Text key={index}>{item}</Text>) }
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +45,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 40,
+    backgroundColor: '#fff',
+  },
+  inputText: {
+    width: 200,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#313131',
+    borderRadius: 4,
+  },
+  button: {
+    color: '#FFFFFF',
+    backgroundColor: '#3A96F4',
+    borderRadius: 4,
   },
 });
